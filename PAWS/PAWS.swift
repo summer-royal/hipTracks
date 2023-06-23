ee335075085aaf6b93588208e645bcf2bc92b1ee
summer-royal <93848318+summer-royal@users.noreply.github.com>
2023-06-23 01:13:57 -1000

    Add files via upload

---
+++ PAWS/PAWS.swift
@@ -0,0 +1,48 @@
+//
+// This source file is part of the CS342 2023 PAWS Team Application project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+import CardinalKit
+import PAWSLandingScreen
+import PAWSMockDataStorageProvider
+import PAWSOnboardingFlow
+import PAWSSharedContext
+import SwiftUI
+
+@main
+struct PAWS: App {
+    @UIApplicationDelegateAdaptor(PAWSAppDelegate.self) var appDelegate
+    @AppStorage(StorageKeys.onboardingFlowComplete) var completedOnboardingFlow = false
+
+    @State var pressedStart = false
+
+    var isSheetPresented: Binding<Bool> {
+        Binding(
+            get: {
+                !completedOnboardingFlow && pressedStart
+            }, set: { _ in }
+        )
+    }
+
+    var body: some Scene {
+        WindowGroup {
+            Group {
+                if completedOnboardingFlow {
+                    HomeView()
+                } else {
+                    LandingScreen(pressedStart: $pressedStart)
+                }
+            }
+                .sheet(isPresented: isSheetPresented) {
+                    OnboardingFlow()
+                        .interactiveDismissDisabled(true)
+                }
+                .testingSetup()
+                .cardinalKit(appDelegate)
+        }
+    }
+}
